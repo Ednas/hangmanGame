8779b7653ab590a9e9e8fb04a3d7dcc23546dc49
Ednas <edna@edna.tech>
2016-09-21 14:12:20 -0400

    Trying to get blanks to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,9 +11,9 @@ var blanksAndGuesses = []; //Guesses and blanks will be in this array
 var blanks = 0; //holds blanks spaces for the word chosen
 
 // Counters
-var winCounter  = 0;
+var winCounter = 0;
 var lossCounter = 0;
-var numGuesses  = 9;   
+var numGuesses = 9;
 
 //array for users guessed letters
 var guessedLetter = [];
@@ -22,54 +22,64 @@ var correctLetter = [];
 
 
 //Below is the functionality of the game
-function startGame(){
-	numGuesses = 9;
-//Selects a fruitword at random
-var currentWord = fruitWords[Math.floor(Math.random() * fruitWords.length)];
-   console.log(currentWord);
-//Splits the current word into letters, so that you can match the users guesses to the letters of the word
-var currentLetters = currentWord.split("");
+function startGame() {
+  numGuesses = 9;
+  //Selects a fruitword at random
+  var currentWord = fruitWords[Math.floor(Math.random() * fruitWords.length)];
+  console.log(currentWord);
+  //Splits the current word into letters, so that you can match the users guesses to the letters of the word
+  var currentLetters = currentWord.split("");
+
+  //Need to know how many blanks
+  var blanks = currentWord.length;
+  console.log(blanks + " Sparkles");
 
 };
 
 
 //once you click a button, makes it lowercase and adds to the user guess element ID
-document.onkeyup = function(event) 
-{
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	console.log(userGuess);
+document.onkeyup = function(event) {
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  console.log(userGuess);
 
-	guessedLetter.push(" " + userGuess);
+  guessedLetter.push(" " + userGuess);
 
-	document.getElementById('guessed').innerHTML = "Letters Already Guessed " + guessedLetter;
-
+  document.getElementById('guessed').innerHTML = "Letters Already Guessed " + guessedLetter;
 
 
-console.log(currentLetters);
 
-//If the users guessed letter = a current letter, it will pop that letter off the array 
-//and once the user has emptied the array, they win. If they use up more than 20 letters, then
-//they lose.
-for (var i = 0; i < currentLetters.length; i++) {
-	currentLetters[i]
+  console.log(currentLetters);
 
-if (currentLetters == userGuess){
-	console.log(userGuess);
+  //If the users guessed letter = a current letter, it will pop that letter off the array 
+  //and once the user has emptied the array, they win. If they use up more than 20 letters, then
+  //they lose.
+
+  // for (var i = 0; i < currentLetters.length; i++) {
+  //   currentLetters[i]
+
+  //   if (currentLetters == userGuess) {
+  //     console.log(userGuess);
+  //   } else {
+  //     console.log("Wrong guess");
+  //   }
+  // };
+
+  
+  // for (var i = currentWord.length - 1; i >= 0; i--) {
+  // 	currentWord[i]
+  // 	var goal = userGuess.match(currentLetters);
+  // console.log(userGuess + "it worked");
+  // currentLetters.pop(userGuess);
+  // console.log(currentLetters);
+  // };
+
+
+  if (userGuess == currentLetters) {
+    console.log("Works");
+  };
 }
-else {
-	console.log("Wrong guess");
-}
-};
-// for (var i = currentWord.length - 1; i >= 0; i--) {
-// 	currentWord[i]
-// 	var goal = userGuess.match(currentLetters);
-// console.log(userGuess + "it worked");
-// currentLetters.pop(userGuess);
-// console.log(currentLetters);
-// };
 
+//Making the stuff work
 
-if (userGuess == currentLetters){
-	console.log("Works");
-};
-}
+//Calling the startGame function
+startGame();
