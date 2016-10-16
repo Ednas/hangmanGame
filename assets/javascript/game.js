59593910bbe2ec44319a71bf72f0943a46999683
Ednas <edna@edna.tech>
2016-10-16 16:10:09 -0400

    Moved the functionality from keyup to the buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 //Made the variables first to use in the functions
 //Array of fruitwords
-var fruitWords = ['banana', 'fig', 'watermelon', 'grape', 'coconut', 'cherry', 'lime', 'pineapple', 
-'tomato', 'apple', 'orange', 'kiwi', 'strawberry', 'dragonfruit', 'blueberry', 'blackberry', 
-'mango', 'plum', 'pear', 'papaya', 'avoccodo', 'pasionfruit', 'current', 'boysenberry', 'quints', 'salmonberry',
-'yuzu', 'grapefruit', 'kumquat'];
+var fruitWords = ['banana', 'fig', 'watermelon', 'grape', 'coconut', 'cherry', 'lime', 'pineapple',
+  'tomato', 'apple', 'orange', 'kiwi', 'strawberry', 'dragonfruit', 'blueberry', 'blackberry',
+  'mango', 'plum', 'pear', 'papaya', 'avoccodo', 'pasionfruit', 'current', 'boysenberry', 'quints', 'salmonberry',
+  'yuzu', 'grapefruit', 'kumquat'
+];
 var blanksAndSuccess = []; //Correct guesses and blanks will be in this array
 var blanks = 0; //holds blanks spaces for the word chosen
 var currentWord = "";
@@ -14,7 +15,8 @@ var lossCounter = 0;
 var numGuesses = 12;
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'
+];
 
 //array for users guessed letters
 var guessedLetter = [];
@@ -23,53 +25,50 @@ var correctLetter = [];
 //guessed letters that are incorrect
 var incorrectGuess = [];
 
-   // create alphabet ul
-  var buttons = function () {
-    //gets the alphabet button id from html
-    var myButtons = document.getElementById('alphabet-btns');
-    //creates unordered list for the letters
-    var letters = document.createElement('ul');
+// create alphabet ul
+var buttons = function() {
+  //gets the alphabet button id from html
+  var myButtons = document.getElementById('alphabet-btns');
+  //creates unordered list for the letters
+  var letters = document.createElement('ul');
 
 
-    //loops through the alphabet
-    for (var i = 0; i < alphabet.length; i++) {
-      //creates li for list
-      let listItem = document.createElement('li');
-      //this creates the button and adds the bootstrap button look to it
-      listItem = document.createElement('BUTTON');
-      listItem.classList.add('btn-primary');
-      //gives each list item the id letter
-//      list.id = 'letter';
-      listItem.innerHTML = alphabet[i];
-      //appends letters to my buttons and list to letters
-      myButtons.appendChild(listItem);
-      listItem.dataset.alphabet = alphabet[i];
-      listItem.onclick = function(){
-        console.log('hello '+listItem.dataset.alphabet)
-      }
-//      letters.appendChild(list);
-      // alphadata = alphabet[i];
-      // listItem.attribute(data.alphadata);
- 
+  //loops through the alphabet
+  for (var i = 0; i < alphabet.length; i++) {
+    //creates li for list
+    let listItem = document.createElement('li');
+    //this creates the button and adds the bootstrap button look to it
+    listItem = document.createElement('BUTTON');
+    listItem.classList.add('btn-primary');
+    //gives each list item the id letter
+    //      list.id = 'letter';
+    listItem.innerHTML = alphabet[i];
+    //appends listIem to my buttons
+    myButtons.appendChild(listItem);
+    listItem.dataset.alphabet = alphabet[i];
+
+
+    listItem.onclick = function() {
+      var userGuess = listItem.dataset.alphabet;
+      console.log('hello ' + userGuess);
+      guessedLetter.push(userGuess);
+      document.getElementById('guessed').innerHTML = "Letters Already Guessed: " + guessedLetter.join(" ");
+      checkLetters(userGuess); // runs the code to check for correctness 
+      round();
+      //  console.log(userGuess);
     }
+
   }
-  buttons();
-
-
-//  var article = document.getElementById('electriccars');
- 
-// article.dataset.columns // "3"
-// article.dataset.indexNumber // "12314"
-// article.dataset.parent // "cars"
-
+}
+buttons();
 
 
 //Below is the functionality of the game
 function startGame() {
   numGuesses = 12;
-  blanksAndSuccess =[];
-    guessedLetter = [];
-    incorrectGuess = [];  
+  blanksAndSuccess = [];
+  guessedLetter = [];
+  incorrectGuess = [];
   //Selects a fruitword at random
   currentWord = fruitWords[Math.floor(Math.random() * fruitWords.length)];
 
@@ -123,14 +122,14 @@ function round() {
   console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
 
   // Update the HTML to reflect the new number of guesses. Also update the correct guesses.
-  document.getElementById("guessesRemaining").innerHTML= "Number Of Guesses Remaining: " + numGuesses;
+  document.getElementById("guessesRemaining").innerHTML = "Number Of Guesses Remaining: " + numGuesses;
   document.getElementById("currentWord").innerHTML = "Find the missing letters: " + blanksAndSuccess.join(" ");
-  document.getElementById("guessed").innerHTML = "Letters already guessed: " +incorrectGuess.join(" "); 
+  document.getElementById("guessed").innerHTML = "Letters already guessed: " + incorrectGuess.join(" ");
 
   // If we have gotten all the letters to match the solution... 
   if (currentLetters.toString() == blanksAndSuccess.toString()) {
     winCounter++; // add to the win counter 
-    document.getElementById("word").innerHTML = "The word was " +currentWord;
+    document.getElementById("word").innerHTML = "The word was " + currentWord;
     alert("You win! The word was " + currentWord); // give the user an alert   
 
     // Update the win counter in the HTML
@@ -141,7 +140,7 @@ function round() {
   // If we've run out of guesses
   else if (numGuesses == 0) {
     lossCounter++; // add to the loss counter 
-        document.getElementById("word").innerHTML = "The word was " +currentWord;
+    document.getElementById("word").innerHTML = "The word was " + currentWord;
 
     alert("You lose. The word was " + currentWord); // give the user an alert
 
@@ -152,20 +151,6 @@ function round() {
 
 }
 
-
-
 //Making the stuff run
-
 //Calling the startGame function
 startGame();
-
-//once you click a button, makes it lowercase and adds to the user guess element ID
-document.onkeyup = function(event) {
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  console.log(userGuess);
-  guessedLetter.push(userGuess);
-  document.getElementById('guessed').innerHTML = "Letters Already Guessed: " + guessedLetter.join(" ");
-  checkLetters(userGuess); // runs the code to check for correctness 
-  round();
-
-};
